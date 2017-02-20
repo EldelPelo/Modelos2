9a3998bf1894a665dd0d136c9a5a19cd5f414242
EldelPelo <cristian94var@gmail.com>
2017-02-20 16:35:01 -0500

    Create sumascartas.py

---
+++ Ejercicios recursividad/sumascartas.py
@@ -0,0 +1,43 @@
+def comparar(lista):
+    if lista[0]==lista[len(lista)-1] and len(lista)!=1:
+        return 1
+    else:
+        if lista[0]!=lista[len(lista)-1] and len(lista)!=1:
+            return comparar(lista[:len(lista)-1])
+        else:
+            if len(lista)==1:
+                return 0
+
+
+def iterarlista(lista):
+    if comparar(lista)== 1 and len(lista)!=1:
+        return False
+    else:
+        if comparar(lista)==0 and len(lista)!=1:
+            return iterarlista(lista[1:])
+        else:
+            if len(lista)==1:
+                return True
+
+def veinteyuna(lista):
+    if iterarlista(lista)==True:
+        return sacarterminosasumar(lista)
+    else:
+        if iterarlista(lista)==False:
+            return "paila mi perro no se puede hacer eso"
+
+def sacarterminosasumar(lista):
+    if len(lista)!=1:
+        return sumar(lista[0])+sacarterminos(lista[1:])
+    else:
+        if len(lista)==1:
+            return sumar(lista[0])
+
+def sumar(lista):
+    if lista[0]=="J" or lista[0]=="j" or lista[0]=="Q" or lista[0]=="q" or lista[0]=="K" or lista[0]=="k":
+        return 10
+    else:
+		if lista[0]=="A" or lista[0]=="a":
+			return 11
+		else:
+			return lista[0]
