e7589c4d1e118e7a51f3b4525a06dbacf6497795
EldelPelo <cristian94var@gmail.com>
2017-02-20 16:23:40 -0500

    Update Palindromo.py

--- Ejercicios recursividad/Palindromo.py
+++ Ejercicios recursividad/Palindromo.py
@@ -1,9 +1,9 @@
-'''
-Created on 9/02/2017
-
-@author: ElDelPelo
-'''
-def palindromo(frase):
-    return frase == frase[::-1]
-
-print(palindromo("oso"))
+def palindromo(cadena):
+    if cadena[0] == cadena[len(cadena)-1] and len(cadena)!=1:
+        return palindromo(cadena[1:len(cadena)-1])
+    else:
+        if cadena[0] != cadena[len(cadena)-1] and len(cadena)!=1:
+            return "la palabra no es palindroma"
+        else:
+            if len(cadena)==1:
+                return "la palabra es palindroma"
